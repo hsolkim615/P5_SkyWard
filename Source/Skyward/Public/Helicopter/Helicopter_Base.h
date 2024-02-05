9d73f8317ec97672e85b7fe1e6818c47e0cdd5ff
hsolkim615 <144770909+hsolkim615@users.noreply.github.com>
2024-02-05 14:24:57 +0900

    240205 KHS - 헬리콥터 베이스 생성
    240205 KHS - 헬리콥터 베이스 생성

---
+++ Source/Skyward/Public/Helicopter/Helicopter_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "Helicopter_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWARD_API AHelicopter_Base : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
